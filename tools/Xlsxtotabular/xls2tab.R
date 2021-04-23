e5229a3aab86ad35fe7b6c511e5cbf2b4be41f16
ColineRoyaux <61148928+ColineRoyaux@users.noreply.github.com>
2021-04-23 15:00:40 +0200

    add quote=F to write.table

--- tools/Xlsxtotabular/xls2tab.R
+++ tools/Xlsxtotabular/xls2tab.R
@@ -6,4 +6,4 @@ library(readxl)
 
 args = commandArgs(trailingOnly = TRUE)
 sheet <- read_excel(args[1],sheet = args[2])
-write.table(sheet,"out.tabular", sep = "\t", row.names = FALSE)
+write.table(sheet,"out.tabular", quote = FALSE, sep = "\t", row.names = FALSE)
